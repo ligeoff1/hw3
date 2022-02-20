6a5eeb2c30c4585f82619f021c71f230a1bb2fc8
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-02-20 22:15:08 +0000

    update to hidden field place ID

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
       def create  
         @post = Post.new(params["post"])
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places"
       end
     
 end
